dc212fe5a1206c0562e8e17ac5d9b49b20a7adea
bluesnake0818 <raidient87@gmail.com>
2022-12-21 06:44:35 +0900

    move routers to app.js

--- App.js
+++ App.js
@@ -11,23 +11,98 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 import Purchases from 'react-native-purchases';
-import Router from './src/navigation/Router';
 import { API_KEY } from './src/constants';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const App: () => React$Node = () => {
+import { NavigationContainer, DarkTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import PaywallScreen from './src/screens/PaywallScreen';
+import UserScreen from './src/screens/UserScreen';
+import WeatherScreen from './src/screens/WeatherScreen'
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const WeatherRouter = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Weather"
+      component={WeatherScreen}
+      options={{
+        title: '✨ Magic Weather',
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const UserRouter = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="User"
+      component={UserScreen}
+      options={{
+        title: 'User',
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Weather"
+        component={WeatherRouter}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'sunny' : 'sunny-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="User"
+        component={UserRouter}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'ios-person-circle' : 'ios-person-circle-outline'} color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const Router = () => {
+  return (
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={TabNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Paywall"
+          component={PaywallScreen}
+          options={{
+            title: '✨ Magic Weather Premium',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const App = () => {
   useEffect(() => {
-    /* Enable debug logs before calling `setup`. */
+    
     Purchases.setDebugLogsEnabled(true);
 
-    /*
-      Initialize the RevenueCat Purchases SDK.
-
-      - appUserID is nil, so an anonymous ID will be generated automatically by the Purchases SDK. Read more about Identifying Users here: https://docs.revenuecat.com/docs/user-ids
-
-      - observerMode is false, so Purchases will automatically handle finishing transactions. Read more about Observer Mode here: https://docs.revenuecat.com/docs/observer-mode
-
-      - useAmazon is false, so it will use the Play Store in Android and App Store in iOS by default.
-      */
     Purchases.configure({ apiKey: API_KEY, appUserID: null, observerMode: false, useAmazon: false });
   }, []);
 
